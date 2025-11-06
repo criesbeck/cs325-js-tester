67ca85fed62d55f65cb663a9707724cf6397f519
criesbeck <chris.riesbeck@gmail.com>
2025-11-06 13:44:55 -0600

    update for extensible matcher

--- solutions.js
+++ solutions.js
@@ -1,5 +1,13 @@
+// How to export solution functions from another file
 // export * from './solutions/warmup.js';
-// export * from './solutions/mapping.js';
-// export * from './solutions/match.js';
 
+// How to import and export the extensible match() function and additional matchers
+// Assumes match() is defined in match-core.js and additional matchers in matchers.js
+//
+// import { addMatchers, match} from './solutions/match-core.js';
+// import * as matchers from './solutions/matchers.js';
+// addMatchers(matchers);
+// export { match};
+
+// Example of exporting directly from solutions.js
 export const sumOfSquares = (x, y) => x + y;
